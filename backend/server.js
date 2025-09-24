632b7b70edf2e64f341705844533ea53bec0452f
ShivamKureel1122 <sunnykureel1122@gmail.com>
2025-09-24 21:33:04 +0530

    updated: server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 // import cors from 'cors'
-import path from "path"
 
 import authRoutes from './routes/authRoutes.js'
 import productRoutes from './routes/productRoutes.js'
@@ -17,8 +16,6 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-const __dirname = path.resolve() // root of the application
-
 // app.use(cors({
 //     origin: "http://localhost:5173",
 //     credentials: true,
@@ -32,16 +29,6 @@ app.use('/api/products', productRoutes)
 app.use('/api/cart', cartRoutes)
 app.use('/api/coupons', couponRoutes)
 app.use('/api/payments', paymentRoutes)
-
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
-
 
 const startServer = async () => {
     try {
