497bfda364c8998e8a314526b067f92555d62d1f
ShivamKureel1122 <sunnykureel1122@gmail.com>
2025-09-24 22:58:24 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-// import cors from 'cors'
+import cors from 'cors'
 
 import authRoutes from './routes/authRoutes.js'
 import productRoutes from './routes/productRoutes.js'
@@ -16,10 +16,10 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-// }))
+app.use(cors({
+    origin: "https://shopease-ecommerce-store.netlify.app/",
+    credentials: true,
+}))
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cookieParser())
