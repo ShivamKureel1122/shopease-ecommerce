2932551b7f6d4d611885f94236144f9c5ed99915
ShivamKureel1122 <sunnykureel1122@gmail.com>
2025-09-24 19:03:15 +0530

    updated: server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import cors from 'cors'
+// import cors from 'cors'
 import path from "path"
 
 import authRoutes from './routes/authRoutes.js'
@@ -19,10 +19,10 @@ const PORT = process.env.PORT || 5000
 
 const __dirname = path.resolve() // root of the application
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+// }))
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true, limit: "10mb" }))
 app.use(cookieParser())
